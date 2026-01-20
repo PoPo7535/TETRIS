60f628c438030231045d8b0710a214c533bbec48
PoPo7535 <143324551+PoPo7535@users.noreply.github.com>
2026-01-20 15:35:34 +0900

    LAST

--- TETRIS/FSM/Fsm.cs
+++ TETRIS/FSM/Fsm.cs
@@ -5,7 +5,6 @@ public abstract class Fsm(SceneManager fsm)
     public abstract void Enter();
 
     public abstract void Update(ConsoleKeyInfo? keyInfo);
-
-
+    
     public abstract void Exit();
 } 
